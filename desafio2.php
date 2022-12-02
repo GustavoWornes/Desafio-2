e4a140981091869026404343cde557dff4959203
Gustavo <79329987+GustavoWornes@users.noreply.github.com>
2022-12-01 23:02:16 -0300

    first commit

---
+++ desafio2.php
@@ -0,0 +1,26 @@
+<?php
+
+
+function quick_sort($array)
+ {
+	$loe = $gt = array();
+	if(count($array) < 2)
+	{
+		return $array;
+	}
+	$pivot_key = key($array);
+	$pivot = array_shift($array);
+	foreach($array as $val)
+	{
+		if($val <= $pivot)
+		{
+			$loe[] = $val;
+		}elseif ($val > $pivot)
+		{
+			$gt[] = $val;
+		}
+	}
+	return array_merge(quick_sort($loe),array($pivot_key=>$pivot),quick_sort($gt));
+}
+
+?>
